49d367a6a3d6c92e7e2586f9a32bd4386ec22e4d
Theophilus Samuel <36473098+ConnectedDot@users.noreply.github.com>
2023-10-15 11:04:37 +0100

    new frontend

---
+++ src/constants.js
@@ -0,0 +1,10 @@
+// export const BASE_URL =
+//   process.env.NODE_ENV === "develeopment"
+//     ? "proshop-mern-backend.vercel.app"
+//     : "";
+// export const BASE_URL = "proshop-mern-backend.vercel.app"; // If using proxy
+export const PRODUCTS_URL = "/api/products";
+export const USERS_URL = "/api/users";
+export const ORDERS_URL = "/api/orders";
+export const PAYPAL_URL = "/api/config/paypal";
+export const BASE_URL = "https://back.oyelami.me/";
